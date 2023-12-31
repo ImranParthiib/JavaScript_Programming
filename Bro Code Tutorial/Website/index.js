4ac7cd6c83da1ec96aaf12fc162b609c8945e8f6
Imran Parthib <imranparthib13@gmail.com>
2024-01-01 01:25:08 +0600

    JavaScript file added

---
+++ Bro Code Tutorial/Website/index.js
@@ -0,0 +1,19 @@
+console.log("I want to be a good Programmer");
+console.log("And Make a Impact to the Society");
+// window.alert("Unsafe Website");
+
+// This is My JavaScript Marathon Day 1
+let summation, num1 =17, num2=21;
+summation = num1 + num2;
+let firstName = "Imran";
+let lastName = "Parthib";
+console.log("I'am",firstName,lastName);
+// same thing we can do using + 
+console.log("I'am"+" "+firstName+" "+lastName);
+let student = true;
+if(student =true){
+     console.log("Student: Yes");
+}
+console.log(summation);
+document.getElementById("p1").innerHTML = "Hey " +firstName+"_";
+document.getElementById("p3").innerHTML = ", Long time No See, Where have you been these days?";
