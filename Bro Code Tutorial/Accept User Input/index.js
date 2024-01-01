4fcf66b94a3afb2f96575e5db0b4b10bf0ce152d
Imran Parthib <imranparthib13@gmail.com>
2024-01-02 03:38:16 +0600

    Revert "Type Conversion"
    This reverts commit 54e5d79b3a1776868c5fa4bd6545ad75013e4607.

--- Bro Code Tutorial/Accept User Input/index.js
+++ Bro Code Tutorial/Accept User Input/index.js
@@ -11,5 +11,11 @@ document.getElementById("mySubmit").oneclick = function(){
     console.log(username)    
 }
 console.log("Hello");
- 
+let username1;
+document.getElementById("mySubmit1").oneclick = function(){
+    username1 = document.getElementById("myText1").value;
+    console.log(username1)    
+}
+console.log("Hello World");
+
 
--- Bro Code Tutorial/Type Cnversion/index.js
+++ Bro Code Tutorial/Type Cnversion/index.js
@@ -1,1 +1,17 @@
- 
+let age = "23";
+age = Number(age);
+age += 1;
+console.log(age, typeof age);
+
+let x = "Pizza";
+let y = "Pizza";
+let z = "Pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(x, typeof x);
