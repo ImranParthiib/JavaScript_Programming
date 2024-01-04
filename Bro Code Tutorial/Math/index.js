76aa1f0080a7c072781f888081d27a1ca16c0382
Imran Parthib <imranparthib13@gmail.com>
2024-01-05 01:25:49 +0600

    Math Js

---
+++ Bro Code Tutorial/Math/index.js
@@ -0,0 +1,20 @@
+let x = 3.14;
+let y = 3.9;
+let z = 3.8;
+let q = 3.9;
+let a = 3.7;
+ 
+x = Math.round(x);
+console.log(x);
+y = Math.floor(y);
+console.log(y);
+z = Math.ceil(z);
+console.log(z);
+q = Math.pow(q, 90);
+console.log(q);
+a = Math.sqrt(a);
+console.log(a);
+ 
+const PI = Math.PI;
+
+console.log(PI);
