02b7628c5e95af03946143bf54d78ac4adcaf572
Imran Parthib <imranparthib13@gmail.com>
2024-01-06 22:25:51 +0600

    MDN Documentation Study and learning

---
+++ MDN Web Docs/documentationStudy.js
@@ -0,0 +1,12 @@
+let myVariable = [1, "Steve", "Watch", 10, "OUT"];
+for (let i = 0; i < myVariable.length; i++) {
+  console.log(myVariable[i]);
+}
+
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+    console.log("Yay, I love chocolate ice cream");
+}
+else {
+    console.log("Awww, but chocolate is my favorite...");
+}
