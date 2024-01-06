4ef294c022479e826e84f4f8ea6335db0567a5c4
Imran Parthib <imranparthib13@gmail.com>
2024-01-06 22:38:25 +0600

    Getting started Js funtion....

---
+++ MDN Web Docs/functions.js
@@ -0,0 +1,9 @@
+function multiply(num1, num2) {
+  let result = num1 * num2;
+  console.log(
+    "The Multiplication of " + num1 + " And " + num2 + " is: " + result
+  );
+  return result;
+}
+
+multiply(10, 20);
