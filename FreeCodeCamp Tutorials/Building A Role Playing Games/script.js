d87df8cf8a4d4e7f65d1c8df08e6865c0263fbf2
Imran Parthib <imranparthib13@gmail.com>
2024-01-24 16:30:37 +0600

    Some Function and Program added

--- FreeCodeCamp Tutorials/Building A Role Playing Games/script.js
+++ FreeCodeCamp Tutorials/Building A Role Playing Games/script.js
@@ -272,5 +272,7 @@ function pickEight() {
 
 function pick(guess) {
   const numbers = [];
-  while (numbers.length < 10) {}
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
 }
--- ProgramizWebsite/FibonacciSeries.js
+++ ProgramizWebsite/FibonacciSeries.js
@@ -1,21 +1,25 @@
 console.log("I am learning JavaScript");
 let num = 5;
+
 function fibonacci(totalTerm) {
   let firstTerm = 0;
   let secondTerm = 1;
   let result = firstTerm + "," + secondTerm;
+
   if (totalTerm <= 0) {
+    console.log(result);
     return 0;
   }
+
   let lastTerm = firstTerm + secondTerm;
-  for (let i = 0; i < totalTerm; i++) {
+  for (let i = 0; i < totalTerm - 2; i++) {
     result += "," + lastTerm;
-    console.log(result);
     firstTerm = secondTerm;
     secondTerm = lastTerm;
     lastTerm = firstTerm + secondTerm;
-    }
-    console.log(result);
+  }
+
+  console.log(result);
 }
 
 fibonacci(10);
