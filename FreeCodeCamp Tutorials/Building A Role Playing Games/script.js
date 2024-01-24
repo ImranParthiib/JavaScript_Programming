e0685cdf2feca8c713ed7df6943e036b297a86e4
Imran Parthib <imranparthib13@gmail.com>
2024-01-24 17:00:08 +0600

    169 step completed

--- FreeCodeCamp Tutorials/Building A Role Playing Games/script.js
+++ FreeCodeCamp Tutorials/Building A Role Playing Games/script.js
@@ -276,5 +276,5 @@ function pick(guess) {
     numbers.push(Math.floor(Math.random() * 11));
   }
   text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
-  for (let i = 0; x < 5; x++) {}
+  for (let i = 0; i < 10; x++) {}
 }
