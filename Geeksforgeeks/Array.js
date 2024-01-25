c6417e8111e892ac3b64ca79e5bca014a0a7ca7b
Imran Parthib <imranparthib13@gmail.com>
2024-01-25 22:12:14 +0600

    Control Flow

--- Geeksforgeeks/Array.js
+++ Geeksforgeeks/Array.js
@@ -3,3 +3,12 @@ let array = [1, 2, 3, 4, 5, 6, 7];
 for (let i = 0; i < array.length; i++) {
   console.log(array[i]);
 }
+
+let j = 0;
+
+let array2 = [1, 2, 3, 4, 5, 6, 7];
+
+while (j < array.length) {
+  console.log(array2[j]);
+  j++;
+}
