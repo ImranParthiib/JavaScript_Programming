9f7b2429af52989dc3bd8e59e345de553ffa23c2
Imran Parthib <imranparthib13@gmail.com>
2024-02-08 21:59:03 +0600

    learning array and objects

---
+++ W3 Schools Documentation/Js Intro/Array/object.js
@@ -0,0 +1,12 @@
+const car = {
+    type: "Fiat",
+    model: "500wD",
+    color: "white"
+};
+
+car.color = "red";
+car.owner = "ImranParthib";
+
+console.log(car);
+console.log(car.owner);
+console.log(car.color);
