dd093317e680e3ab5f5625d192c3d0b62a8f7d26
Imran Parthib <imranparthib13@gmail.com>
2024-02-08 22:22:08 +0600

    playing with js

--- W3 Schools Documentation/Js Intro/Array/object.js
+++ W3 Schools Documentation/Js Intro/Array/object.js
@@ -10,3 +10,15 @@ car.owner = "ImranParthib";
 console.log(car);
 console.log(car.owner);
 console.log(car.color);
+
+let a = 3;
+let b = 4;
+
+let result = 3 ** 4;
+
+console.log(result);
+
+let text1 = "A";
+let text2 = "B";
+let result2 = text1 < text2;
+console.log(result2);
