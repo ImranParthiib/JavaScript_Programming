575a8ab1a834f066f1c5aaa868516c8d534599fe
ImranParthib <imranparthib13@gmail.com>
2024-03-04 23:08:09 +0600

    string template

--- W3 Schools Documentation/Js Intro/Methods In Object/Person.js
+++ W3 Schools Documentation/Js Intro/Methods In Object/Person.js
@@ -8,3 +8,16 @@ const person = {
 };
 
 console.log(person.fullName());
+
+const date = new Date("Mar 25 2015");
+console.log(date);
+
+const biography = `🚀 Enthusiastic Web Developer and programmer 🌐 Crafting seamless digital experiences with passion and precision. Proudly representing the vibrant spirit of Bangladesh! 🇧🇩💻 #CodeCreator
+
+💡 I love transforming ideas into reality through code. My journey in the world of programming is an ever-evolving exploration, and I thrive on challenges that push me to learn and grow.
+
+🌱 Currently learning and exploring new technologies to stay ahead in the dynamic field of web development. Excited about the potential of AI, blockchain, and their intersection with web technologies.
+
+📫 Connect with me! Let's discuss ideas, collaborate on projects, or just chat about the latest tech trends. Open to exciting opportunities and collaborations.`;
+
+console.log(biography);
