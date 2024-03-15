ee4e9dd9a69b0f34e0cc5639b31bd6987821cd3a
ImranParthib <imranparthib13@gmail.com>
2024-03-15 21:29:23 +0600

    🚧 String Js

---
+++ LWS Tutorial/JS String/string.js
@@ -0,0 +1,13 @@
+let text = "Imran Parthib a last year student of CSE Departement";
+console.log(text.length);
+
+let longText = `This is Imran Parthib,having knowledge in multiple \"|language|\" and \"|framewokrks|\".`;
+console.log(longText);
+
+let clubName = "Bercelona";
+
+let fPlayer = new String("Messi");
+
+// Don't create strings as an Objects. It slows down the execution speed.
+console.log(typeof (clubName));
+console.log(typeof (fPlayer));
