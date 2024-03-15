fc051407f9b7fee60cacfbeed9bfd7c0188aac1e
ImranParthib <imranparthib13@gmail.com>
2024-03-15 20:21:54 +0600

    🚀 Events in Js

---
+++ LWS Tutorial/Event/script.js
@@ -0,0 +1,8 @@
+function showDate() {
+  alert("Do you Know!");
+  document.getElementById("demo").innerHTML = Date();
+}
+
+function showHover() {
+  document.getElementById("test").innerHTML = "Testing";
+}
