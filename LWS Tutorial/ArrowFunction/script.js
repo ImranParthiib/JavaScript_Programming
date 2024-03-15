ed940abf5f106fc1fd815bdd5fc174f2a12788b6
ImranParthib <imranparthib13@gmail.com>
2024-03-16 00:02:13 +0600

    ✨ Arrow Function

---
+++ LWS Tutorial/ArrowFunction/script.js
@@ -0,0 +1,22 @@
+hello = () => "Hello World";
+
+console.log(hello());
+
+let numAddition = (a, b) => a + b;
+
+let result = numAddition(23, 54);
+console.log(result);
+
+//* subtraction using Arrow Function
+
+let numSubtraction = (a, b) => a - b;
+
+let subResult = numSubtraction(5345, 435643);
+console.log(subResult);
+
+greet = function () {
+    return "How You have been?";
+}
+
+let greeting = greet();
+console.log(greeting);
