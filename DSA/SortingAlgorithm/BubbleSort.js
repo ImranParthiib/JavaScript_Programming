3ceeac9d32045e3e02f3488c1027080679585bac
ImranParthib <imranparthib13@gmail.com>
2024-05-23 01:56:59 +0600

    typechecked

---
+++ DSA/SortingAlgorithm/BubbleSort.js
@@ -0,0 +1,11 @@
+let myArray = [1, 34, 5, 6, 6, 67, 78];
+console.log("This is my Initial Array:");
+let output = "";
+for (let i = 0; i < myArray.length; i++) {
+  output += myArray[i] + " ";
+}
+console.log(output);
+let str = "Imran Parthib";
+console.log(typeof str);
+console.log(typeof myArray);
+
---
+++ DSA/SortingAlgorithm/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log("This is my Initial Array:");
+// let output = "";
+// for (let i = 0; i < myArray.length; i++) {
+//   output += myArray[i] + " ";
+// }
+// console.log(typeof output);
