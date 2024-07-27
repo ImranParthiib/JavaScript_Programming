95939beaacaad82283029f3d89df5d70dcdacd95
ImranParthib <imranparthib13@gmail.com>
2024-07-27 18:22:59 +0600

    ✨chore: Add inner-section class to actions section for consistent styling

--- Programming Hero/Module 24/DOM/js/script.js
+++ Programming Hero/Module 24/DOM/js/script.js
@@ -33,3 +33,30 @@ const h2 = document.querySelectorAll("h2");
 for (const header of h2) {
   header.classList.add("color");
 }
+
+const newLi = document.createElement("li");
+newLi.textContent = "Rakib";
+document.querySelector("ul").appendChild(newLi);
+
+// Create a new section after the Shopping list section
+const newSection = document.createElement("section");
+newSection.id = "new-section";
+
+// Insert the new section after the Shopping list section
+const shoppingListSection = document.getElementById("item-list");
+shoppingListSection.insertAdjacentElement("afterend", newSection);
+
+newSection.style.backgroundColor = "lightblue";
+newSection.style.fontSize = "26px";
+
+const img = document.createElement("img");
+img.src =
+  "https://media.licdn.com/dms/image/D5622AQEZP4HuFXGNDw/feedshare-shrink_800/0/1695801323000?e=2147483647&v=beta&t=b_lTOSRIi3mscHYu1b0K-JRvldAT4Mj4sJT0ufpvOvU";
+img.alt = "Random Image";
+img.style.width = "100%";
+img.style.height = "500px";
+newSection.appendChild(img);
+
+if (window.innerWidth <= 768) {
+  img.style.height = "200px";
+}
