067cf3ee7fe90fd0d63e60516c9d8dba1cb97074
ImranParthib <imranparthib13@gmail.com>
2024-08-23 15:12:06 +0600

    🚀 feat: Add fetch API code to display user list

---
+++ Programming Hero/Module 33/js/fetch.js
@@ -0,0 +1,23 @@
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+const user = "https://jsonplaceholder.typicode.com/users";
+
+fetch(user)
+  .then((res) => res.json())
+  .then((user) => displayUser(user));
+
+let userName = [];
+function displayUser(user) {
+  const userList = document.getElementById("user-list");
+  for (const users of user) {
+    const li = document.createElement("li");
+    li.textContent = users.name;
+    li.setAttribute(
+      "style",
+      "list-style-type: none; background: #273f43; width:11%;  color:white; margin:10px; padding:5px; border-radius: 5px;"
+    );
+    userList.appendChild(li);
+  }
+}
