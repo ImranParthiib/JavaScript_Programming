934defc4543066e67701c4411a49bb7258effdac
ImranParthib <imranparthib13@gmail.com>
2024-09-26 00:56:23 +0600

    ✨ Refactor nthFibonacci algorithm to calculate the nth term recursively

---
+++ DSA/A Simple Algorithm/nthFibonacci.js
@@ -0,0 +1,12 @@
+const fibonacci = (n) => {
+    if (n <= 1) {
+        return n;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+};
+
+const nthfibo = fibonacci(6);
+
+console.log(nthfibo);
