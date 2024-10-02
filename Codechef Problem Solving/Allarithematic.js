7e636b50fbca03069281af9502d5dc24f7534ba8
ImranParthib <imranparthib13@gmail.com>
2024-10-02 19:10:10 +0600

    ✨ Add Allarithematic.js and Print Squares.js for Codechef problem solving

---
+++ Codechef Problem Solving/Allarithematic.js
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/javascript/LPJAS02/problems/LPJSPR21
+
+for (let i = 1; i < 5; i++) {
+  switch (i) {
+    case 1:
+      console.log(8 + 4);
+      break;
+    case 2:
+      console.log(8 - 4);
+      break;
+    case 3:
+      console.log(8 / 4);
+      break;
+    case 4:
+      console.log(8 * 4);
+      break;
+    default:
+      console.log("Arithematic operations");
+      break;
+  }
+}
---
+++ Codechef Problem Solving/Print Squares.js
@@ -0,0 +1,4 @@
+// https://www.codechef.com/practice/course/javascript/LPJAS02/problems/LPJSPR20
+for (let i = 1; i < 6; i++) {
+  console.log(`${i} - ${i * i}`);
+}
