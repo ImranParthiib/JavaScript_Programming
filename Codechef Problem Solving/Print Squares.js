dbdf0385eedc0e79a2b1117167357c71587bed45
ImranParthib <imranparthib13@gmail.com>
2024-10-20 02:37:14 +0600

    🚀 Refactor Print Squares.js to use readline for user input and while loop for printing squares of numbers

--- Codechef Problem Solving/Print Squares.js
+++ Codechef Problem Solving/Print Squares.js
@@ -1,6 +1,24 @@
+const readline = require("readline");
+
 // https://www.codechef.com/practice/course/javascript/LPJAS11/problems/LPJSPR121
-let N = 5;
-while (N > 0) {
-    console.log(N * N);
-    N--;
-}
+// let N = parseInt(inputChar);
+
+// Write your code here
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question( (input) => {
+  const N = parseInt(input);
+  let y = 1;
+  let result = [];
+
+  while (y <= N) {
+    result.push(y * y);
+    y++;
+  }
+
+  console.log(result.join(" "));
+  rl.close();
+});
