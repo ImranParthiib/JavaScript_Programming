c3766a70cd6b193a1135bb2fd521e3575f656c70
ImranParthib <imranparthib13@gmail.com>
2024-10-25 02:49:09 +0600

    ✨ Refactor .gitignore to include fire.png as favicon and update page title

--- Programming Hero/Milestone 9/Module 49/FirstFirebaseApp/src/main.jsx
+++ Programming Hero/Milestone 9/Module 49/FirstFirebaseApp/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
